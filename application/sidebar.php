30529a732586e4551334d600ae314a67c024e635
mvasco <modestovasco@gmail.com>
2016-10-25 13:40:08 -0600

    021201610251334 ARCHIVOS BASE

---
+++ application/sidebar.php
@@ -0,0 +1,132 @@
+   <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+              <!-- Sidebar user panel -->
+              <div class="user-panel">
+                <div class="pull-left image">
+                  <img src="../../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                </div>
+                <div class="pull-left info">
+                  <p>Alexander Pierce</p>
+                  <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+                </div>
+              </div>
+              
+              <!-- sidebar menu: : style can be found in sidebar.less -->
+              <ul class="sidebar-menu">
+              <li class="header">DATOS GENERALES</li>
+                <li>
+                  <a href="agencias.php">
+                    <i class="fa fa-sitemap"></i> <span>Agencias</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-group"></i> <span>Asesores</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-shield"></i> <span>Aseguradoras</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-credit-card"></i> <span>Créditos</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-phone"></i> <span>Auxilio Vial</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-car"></i> <span>Autos nuevos/seminuevos</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li class="header">OPCIONES DE LA APP</li>
+                <li class="header">Citas a Servicio</li>
+               
+                
+                  <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-car"></i> <span>Vehículos</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-calendar"></i> <span>Años</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-tachometer"></i> <span>Kilómetros</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-cubes"></i> <span>Tipos de citas</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li class="header">Trámites Online</li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-cubes"></i> <span>Tipos de trámites</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                <li class="header">Costo de Servicios</li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-car"></i> <span>Vehículos</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-tachometer"></i> <span>Kilómetros</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-calendar"></i> <span>Años</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                  <li class="header">DATOS RECIBIDOS</li>
+                <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-calendar"></i> <span>Citas a Servicio</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+              <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-cubes"></i> <span>Trámites online</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 <li>
+                  <a href="asesores.php">
+                    <i class="fa fa-money"></i> <span>Costo de Servicios</span>
+                    <small class="label pull-right bg-red">3</small>
+                  </a>
+                </li>
+                 
+                
+                
+                   
+              </ul>
+            </section>
+            <!-- /.sidebar -->
