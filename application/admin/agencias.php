25c136741a1e822d40b6c462fada2a1062d073a9
mvasco <modestovasco@gmail.com>
2016-10-25 18:28:02 -0600

    021 en pruebas

--- application/admin/agencias.php
+++ application/admin/agencias.php
@@ -95,7 +95,7 @@
                 <div class="modal-dialog modal-md">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <button type="button" class="close" data-dismiss="modal">×</button>
+                      <button type="button" class="close" data-dismiss="modal">X</button>
                       <h4 class="modal-title">Form Master Customer</h4>
                     </div>
                     <!--modal header-->
